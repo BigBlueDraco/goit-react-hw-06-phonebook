6644c2df24040054eab529e0fd1de555e0339046
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-10-27 20:08:37 +0300

    add middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { contactReducer } from './contactSlice';
 import storage from 'reduxjs-toolkit-persist/lib/storage';
 import { persistReducer, persistStore } from 'reduxjs-toolkit-persist';
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
 
 const reducers = combineReducers({
   contacts: contactReducer,
@@ -16,6 +17,11 @@ const _persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
   reducer: _persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: false,
+    },
+  }),
 });
 
 export const persistor = persistStore(store);
